87c8f36ef70980f8d4ebef0c7362349e39c5ec90
phpfontana <p.h.p.fontana@gmail.com>
2026-03-23 15:56:35 -0300

    fix download script

--- make_dataset.py
+++ make_dataset.py
@@ -1,17 +1,18 @@
 import pystac
 import stac_asset.blocking
+from tqdm import tqdm
 
-# Load the collection
 collection = pystac.Collection.from_file(
     "https://capella-open-data.s3.us-west-2.amazonaws.com/stac/capella-open-data-ieee-data-contest/collection.json"
 )
 
-# Get the first item
 item_links = collection.get_item_links()
 item = pystac.Item.from_file(item_links[0].absolute_href)
 
-# Download a single specific asset (e.g. "thumbnail")
 config = stac_asset.blocking.Config(include=["thumbnail"])
-item = stac_asset.blocking.download_item(item, config=config)
 
-print("Downloaded to:", list(item.assets["thumbnail"].href))
+with tqdm(total=1, desc="Downloading thumbnail", unit="file") as pbar:
+    item = stac_asset.blocking.download_item(item, directory=".", config=config)
+    pbar.update(1)
+
+print("Done!")
